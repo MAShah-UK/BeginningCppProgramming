34cb7209c3dc0628a0428ecdc930ebd048358f67
M. Shah <m.a.shah@live.co.uk>
2018-08-10 20:36:07 +0100

    Created and ran first project.
    The course strayed from convention as the student wasn't asked to write a 'hello world' application. Despicable.

---
+++ sections/Getting Started/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int favourite_number;
+    std::cout << "Enter you favourite number between 1 and 100: ";
+    std::cin >> favourite_number;
+    std::cout << "Amazing that's my favourite number too." << std::endl;
+    return 0;
+}
