fb7528d13d7b9bf4f7473403cc811bbf1fa5194a
MAShah-UK <m.a.shah@live.co.uk>
2018-08-15 18:44:33 +0100

    Completed two operator challenges, completed 10 question quiz, and completed the section.

--- sections/Statements and Operators/main.cpp
+++ sections/Statements and Operators/main.cpp
@@ -91,9 +91,9 @@ int main() {
     a *= 2; // a = a * 2. a is now 10.
     a /= 2; // a = a / 2. a is now 5.
     a %= 3; // a = a % 3. a is now 2.
-    // It also works with bitwise operators.
+    // Bitwise operators are also supported.
 
-    
+
 
     return 0;
 }
@@ -139,5 +139,112 @@ int arithmetic_operators(int number) {
     return number;
 }
 
+Challenge 3: Logical operators and operator precedence.
+#include <iostream>
+using namespace std;
+void logical_operators(int age, bool parental_consent, bool ssn, bool accidents) {
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    if (((age >= 18) || (age > 15 && parental_consent)) && ssn && !accidents)//WRITE ALL YOUR CODE WITHIN THE PARENTHESES
+        cout << "Yes, you can work.";
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+    return;
+}
+
+Challenge 4: Compound assignment operators.
+#include <iostream>
+using namespace std;
+int main() {
+  const int cents_dollar {100};
+  const int cents_quarter {25};
+  const int cents_dime {10};
+  const int cents_nickel {5};
+  const int cents_penny {1};
+
+  int cents_input {};
+  int balance {};
+  int change_dollars {};
+  int change_quarters {};
+  int change_dimes {};
+  int change_nickels {};
+  int change_pennies {};
+
+  cout << "Enter an amount in cents: ";
+  cin >> cents_input;
+  balance = cents_input;
+
+  change_dollars = balance / cents_dollar;
+  balance %= cents_dollar;
+  change_quarters = balance / cents_quarter;
+  balance %= cents_quarter;
+  change_dimes = balance / cents_dime;
+  balance %= cents_dime;
+  change_nickels = balance / cents_nickel;
+  balance %= cents_nickel;
+  change_pennies = balance;
+  balance = 0;
+
+  cout << "Dollars: " << change_dollars << endl;
+  cout << "Quarters: " << change_quarters << endl;
+  cout << "Dimes: " << change_dimes << endl;
+  cout << "Nickels: " << change_nickels << endl;
+  cout << "Pennies: " << change_pennies << endl;
+}
+
+Quiz 1: The ________ operator increments the value of its operand by one then uses the value.
+Prefix increment.
+
+Quiz 2: The operator used to test two operands for equality is ________.
+==
+
+Quiz 3: The operator that represents logical AND is ________.
+&&
+
+Quiz 4: The operator that represents logic OR is ________.
+||
+
+Quiz 5: If a is 3, b is 5, and c is 7, which of the following are true?
+b == 5;             // true
+c == (a + b) – 1;   // true
+b > 5;              // false
+a >= 3;             // true
+c <= (a * b) / a;   // false
+
+Quiz 6: a < 10  is called a(n) ________.
+Relational expression.
+
+Quiz 7: What will the following code display?
+int x = 10, y = 3, z = 6;
+cout << (x == y) << " ";
+cout << (z <= x) << " ";
+cout << (y != z) << " ";
+cout << (x > y && z < x) << " ";
+cout << (y < x && z < x) << " ";
+cout << (x < y || z < 0) << endl;
+
+0 1 1 1 1 0
+
+Quiz 8: What is displayed by the following code?
+int amount = 100;
+amount += amount * 2;
+cout << amount << endl;
+
+300
+
+Quiz 9: What is displayed by the following code?
+int x= 5;
+int y= -2;
+int z = 2;
+cout << (x + y * z <= x + z * z - x) << endl;
+
+1 because 1 <= 4 is true.
+
+Quiz 10: What are the values of x, y and z after the following code executes?
+double  a = 100;
+int b = 12;
+int x = b % 3;
+double  y = a / b;
+int z = a / b;
+
+x = 0, y = 8.33..., z = 8
 
  */
