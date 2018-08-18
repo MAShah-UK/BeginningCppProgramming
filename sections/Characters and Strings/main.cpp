0d5b1f2cbb1881691b83aa7dd78dfd99552497f7
MAShah-UK <m.a.shah@live.co.uk>
2018-08-18 13:38:10 +0100

    Completed challenge, 10 part quiz, and assignment for C++ strings, and completed the section.

--- sections/Characters and Strings/main.cpp
+++ sections/Characters and Strings/main.cpp
@@ -109,4 +109,80 @@ void cpp_strings() {
     //----DO NOT MODIFY THE CODE BELOW THIS LINE----
     cout << journal_entry_1 << "\n" << journal_entry_2;
 }
+
+Challenge 4: Encrypt and then decrypt an input string.
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  cout << "Enter a message to encrypt: ";
+  string input;
+  getline(cin, input);
+
+  string encrypted_string;
+  for(size_t i {0}; i < input.length(); i++) {
+    char symbol = input[i];
+    encrypted_string.push_back(symbol + i);
+  }
+  cout << "Encrypted message: " << encrypted_string << endl;
+
+  string decrypted_string;
+  for(size_t i {0}; i < encrypted_string.length(); i++) {
+    char symbol = encrypted_string[i];
+    decrypted_string.push_back(symbol - i);
+  }
+  cout << "Decrypted message: " << decrypted_string << endl;
+}
+
+Quiz 1: C-style strings are terminated with a(n) ________.
+Null character \0.
+
+Quiz 2: The function we can use to copy one C-style string to another is ________?
+strcpy
+
+Quiz 3: In order to use C-style string functions we must #include ________.
+<cstring>
+
+Quiz 4: To convert a char to its upper case equivalent we can use the ________ function.
+toupper
+
+Quiz 5: The C-style string name[6] can ________.
+Contain the name "Curly".
+
+Quiz 6: In order to use a C++ string object, we must #include ________.
+<string>
+
+Quiz 7: The + operator allows us to ________ two C++ strings.
+Concatenate.
+
+Quiz 8: In order to find the index of a substring within a C++ string, we can use the ________ method.
+find
+
+Quiz 9: We can compare two C++ strings using the ________.
+== operator.
+
+Quiz 10: Which of the following are true about C++ strings?
+All of the above - objects, useful methods, C++ style preferred over C-style, and dynamic.
+
+Assignment: Create a string pyramid from the user's input.
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  cout << "Enter the string that will be converted into a pyramid: ";
+  string input;
+  getline(cin, input);
+  for(size_t i {}; i < input.length(); i++) {
+    int diff = input.length()-i-1;
+    string whitespace(diff, ' ');
+    cout << whitespace;
+    cout << input.substr(0, i+1);
+    for(int j {i-1}; j >= 0; j--) {
+      cout << input.at(j);
+    }
+    cout << endl;
+  }
+  return 0;
+}
+
  */
